c399c623ec7d4d80066adac415a31d838c7226d7
toufiq007 <t.limon2342@gmail.com>
2025-03-25 01:08:11 +0600

    find duplicate from an array

---
+++ arrays/removeDuplicate.js
@@ -0,0 +1,30 @@
+
+// remove duplicate from the sorted array
+
+const arr = [1,2,2,3,3,3,5,6]
+// approach one
+// const removeDuplicate = (array)=>{
+//     let uniqueItems = []
+//     for (let i=0; i<array.length; i++){
+//         if (!uniqueItems.includes(array[i])){
+//             uniqueItems.push(array[i])
+//         }
+//     }
+//     return uniqueItems
+// }
+
+
+// approach two
+const findDuplicate = (array)=>{
+    let uniqueItems = []
+    let seen = {}
+    for(let i=0; i<array.length; i++){
+        if (!seen[array[i]]){
+            uniqueItems.push(array[i])
+            seen[array[i]] = true
+        }
+    }
+    return uniqueItems
+}
+const items = findDuplicate(arr)
+console.log(items)
