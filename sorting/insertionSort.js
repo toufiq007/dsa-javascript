d5e4292ccf9aa1dd86859e99851658b642e68daf
toufiq007 <t.limon2342@gmail.com>
2025-04-05 05:48:31 +0600

    insertion sort breakdown

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,12 +1,23 @@
 
 // implementation of insertion sort
-const arry = [5,4,0,1,6,2]
-const n = arry.length
+const array = [5,4,0,1,6,2]
+console.log(array,'before sorted')
+/**
+ * array[0] --> sorted sublist                        --> [5]       --> decrement loop
+ * array[1] --> end of the array --> unsorted sublist --> [4,0,1,6,2] --> increment loop
+ * then run a loop in the unsorted sublist
+ * another loop for sorted sublist
+ * then compare the temp variable to the sortedSublist element if sortedSublist element is greather than temp then shift the position by increment one
+ */
+
+
+
+const n = array.length
 const insertionSort = (array,n)=>{
     for (let i=1; i<n; i++){
-        temp = array[i]
-        j = i-1;
-        while(j>=0 && array[j] > temp){
+        let temp = array[i] // i --> 1 
+        let j = i-1         // j = i-1 --> 0    
+        while (j>=0 && array[j] > temp){
             array[j+1] = array[j]
             j--
         }
@@ -14,5 +25,5 @@ const insertionSort = (array,n)=>{
     }
 }
 
-insertionSort(arry,n)
-console.log(arry,'after sort');
+insertionSort(array,n)
+console.log(array,'after sorted')
