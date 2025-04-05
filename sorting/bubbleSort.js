e6cbd9e2db1c33fd57be57204431960a8c1f7041
toufiq007 <t.limon2342@gmail.com>
2025-04-05 06:07:26 +0600

    simplify the swap method

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -18,19 +18,22 @@ const bubbleSort = (arr, n) => {
   for (let i = 0; i < n - 1; i++) {
     // this loop is for number of passes and it should be gone to n-1 times
     // this inner loop is for comparing the pares
-    swap = false
+    swap = false;
     for (let j = 0; j < n - 1 - i; j++) {
       // now compare the pairs
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+        // let temp = arr[j + 1];
+        // arr[j + 1] = arr[j];
+        // arr[j] = temp;
+
+        // simplify swap with destructuring
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
         swap = true;
       }
     }
     if (swap == false) {
       console.log("array is sorted");
-      break
+      break;
     }
   }
 };
